55fd478cf7aef8207c470e90032c495655fe30da
Sefa Pamuk <sefa76q@hotmail.de>
2022-11-23 14:11:27 +0100

    new runs

--- getdist/photo_opt.py
+++ getdist/photo_opt.py
@@ -28,7 +28,7 @@ gauss3=GaussianND(mean=mp1.fiducials, cov=mp1.fishermatrix,names=mp1.paramnames,
 ## Cosmo Triangle
 g = plots.get_subplot_plotter(settings=plot_settings)
 #g.triangle_plot([mcmc1.samples,gauss1,gauss2],filled=[True,False,False],params=cf1.cosmonames,legend_labels=['MCMC','CF-class','CF-camb'],contour_lws=[1,2,2])
-g.triangle_plot([gauss1,gauss2],filled=[False,False],params=cf1.cosmonames,legend_labels=['CF-class','CF-camb'],contour_lws=[2,2])
+g.triangle_plot([gauss1,gauss2,gauss3],filled=[False,False,False],params=cf1.cosmonames,legend_labels=['CF-class','CF-camb','MP-Fish'],contour_lws=[2,2,2])
 g.export('./WLxGCPh_Opt_cosmo.pdf')
 print('Cosmo triangle finished')
 del g
@@ -36,7 +36,7 @@ del g
 ## Nuisance Triangle
 g = plots.get_subplot_plotter(settings=plot_settings)
 #g.triangle_plot([mcmc1.samples,gauss1,gauss2],filled=[True,False,False],params=cf1.nuisancenames,legend_labels=['MCMC','CF-class','CF-camb'],contour_lws=[1,2,2])
-g.triangle_plot([gauss1,gauss2],filled=[False,False],params=cf1.nuisancenames,legend_labels=['CF-class','CF-camb'],contour_lws=[2,2])
+g.triangle_plot([gauss1,gauss2,gauss3],filled=[False,False,False],params=cf1.nuisancenames,legend_labels=['CF-class','CF-camb','MP-Fish'],contour_lws=[2,2,2])
 g.export('./WLxGCPh_Opt_nuisance.pdf')
 print('Nuisance triangle finished')
 del g
@@ -44,7 +44,7 @@ del g
 ## Cosmo-Nuisance Rectangle
 g = plots.get_subplot_plotter(settings=plot_settings)
 #g.rectangle_plot(cf1.cosmonames,cf1.nuisancenames,roots=[mcmc1.samples,gauss1,gauss2],filled=[True,False,False],legend_labels=['MCMC','CF-class','CF-camb'],contour_lws=[1,2,2])
-g.rectangle_plot(cf1.cosmonames,cf1.nuisancenames,roots=[gauss1,gauss2],filled=[False,False],legend_labels=['CF-class','CF-camb'],contour_lws=[1,2,2])
+g.rectangle_plot(cf1.cosmonames,cf1.nuisancenames,roots=[gauss1,gauss2,gauss3],filled=[False,False,False],legend_labels=['CF-class','CF-camb','MP-Fish'],contour_lws=[2,2,2])
 g.export('./WLxGCPh_Opt_cross.pdf')
 print('Cross-terms rectangle finished')
 del g
